3d8ede7656f9ae1a94f0e3039a71d1719ab0c31d
fmdevelop <mail@fmdevelop.com>
2015-12-13 11:23:06 +0100

    Finished basic game logic, except explosions

---
+++ src/gameplay/initialgamestategenerator.cpp
@@ -0,0 +1,6 @@
+#include "initialgamestategenerator.h"
+
+InitialGameStateGenerator::InitialGameStateGenerator()
+{
+
+}
---
+++ src/gameplay/initialgamestategenerator.h
@@ -0,0 +1,11 @@
+#ifndef INITIALGAMESTATEGENERATOR_H
+#define INITIALGAMESTATEGENERATOR_H
+
+
+class InitialGameStateGenerator
+{
+public:
+    InitialGameStateGenerator();
+};
+
+#endif // INITIALGAMESTATEGENERATOR_H
